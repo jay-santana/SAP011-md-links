e4c331f852b5a4d1f31406cd47f4671ce19b8dcd
jay santana <jayannyrayara@gmail.com>
2023-10-27 22:17:15 -0300

    Finalização testes unitários função mdLinks

--- src/md-links.js
+++ src/md-links.js
@@ -7,31 +7,31 @@ function mdLinks(filePath, options) {
   const fileExtension = path.extname(filePath);
   return new Promise((resolve, reject) => {
     if (fileExtension !== '.md') {
-      reject(new Error('Erro: A extensão do arquivo não é .md'));
+      reject(new Error('A extensão do arquivo não é .md'));
     } else {
       fs.promises.readFile(filePath, 'utf8')
         .then((data) => {
           const links = extractLinks(data, filePath);
           if (options.validate === true && options.stats === true) {
             if (links.length === 0) {
-              reject(new Error('Erro: O arquivo não possui links'));
+              reject(new Error('O arquivo não possui links'));
             } else {
               validateLinks(links)
                 .then((validatedLinks) => {
                   const stats = statsLinks(validatedLinks);
                   resolve(stats);
                 })
-                .catch((err) => reject(new Error(`Erro na validação de links - ${err.message}`)));
+                .catch(() => reject(new Error(`Erro na validação de links`)));
             }
           } else if (options.validate === true) {
             if (links.length === 0) {
-              reject(new Error('Erro: O arquivo não possui links'));
+              reject(new Error('O arquivo não possui links'));
             } else {
               resolve(validateLinks(links));
             }
           } else if (options.stats === true) {
             if (links.length === 0) {
-              reject(new Error('Erro: O arquivo não possui links'));
+              reject(new Error('O arquivo não possui links'));
             } else {
               resolve(statsLinks(links));
             }
@@ -39,7 +39,7 @@ function mdLinks(filePath, options) {
             resolve(links);
           }
         })
-        .catch((err) => reject(new Error(`Erro: O arquivo não pôde ser lido - ${err.message}`)));
+        .catch(() => reject(new Error(`O arquivo não pôde ser lido`)));
     }
   });
 }
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -2,11 +2,11 @@ const { mdLinks } = require('../src/md-links');
 const fs = require('fs');
 const { extractLinks, validateLinks, statsLinks } = require('../src/index');
 
-jest.mock('fs', () => {
-  return {
-    readFile: jest.fn()
-  }
-});
+jest.mock('fs', () => ({
+  promises: {
+    readFile: jest.fn(),
+  },
+}));
 
 jest.mock('../src/index', () => {
   return {
@@ -15,37 +15,107 @@ jest.mock('../src/index', () => {
     statsLinks: jest.fn()
   };
 });
-
 describe('mdLinks', () => {
+  let data; // Variável comum para armazenar o conteúdo do arquivo
+  beforeEach(() => { // Define esses valores comuns antes de cada teste
+    data = '[link](https://example.com)';
+    fs.promises.readFile.mockResolvedValue(data);
+  });
+  
   it('deve rejeitar se a extensão do arquivo não for .md', () => {
-    return expect(mdLinks('arquivo.txt', {})).rejects.toThrow('Erro: A extensão do arquivo não é .md');
+    return expect(mdLinks('arquivo.txt', {})).rejects.toThrow('A extensão do arquivo não é .md');
   });
 
-  // it('deve resolver com os links se options.validate e options.stats forem falsos', () => {
-  //   // Configurar mocks e valores de retorno
-  // });
+  it('deve resolver com os links se options.validate e options.stats forem falsos', () => {
+    extractLinks.mockReturnValue(['https://example.com']);
+    return expect(mdLinks('arquivo.md', {})).resolves.toEqual(['https://example.com']);
+  });
 
-  // it('deve resolver com os links validados se options.validate for verdadeiro', () => {
-  //   // Configurar mocks e valores de retorno
-  // });
+  it('deve rejeitar se o arquivo não puder ser lido', () => {
+    // Simula um erro ao ler o arquivo
+    fs.promises.readFile.mockRejectedValue(new Error('Erro ao ler o arquivo'));
+    return expect(mdLinks('arquivo.md', {})).rejects.toThrow('O arquivo não pôde ser lido');
+  });
 
-  // it('deve resolver com estatísticas se options.stats for verdadeiro', () => {
-  //   // Configurar mocks e valores de retorno
-  // });
+  it('deve resolver com os links validados se options.validate for verdadeiro', () => {
+    const links = ['https://example.com'];
+    extractLinks.mockReturnValue(links);
+    // Mock para simular a validação dos links
+    validateLinks.mockResolvedValue(links.map((link) => ({
+      text: 'Link de exemplo',
+      url: link,
+      statusCode: 200,
+      statusText: 'OK'
+    })));
+    return expect(mdLinks('arquivo.md', { validate: true })).resolves.toEqual([
+      {
+        text: 'Link de exemplo',
+        url: 'https://example.com',
+        statusCode: 200,
+        statusText: 'OK'
+      }
+    ]);
+  });
+  it('deve rejeitar se o arquivo não possui links e options.validate for verdadeiro', () => {
+    // Simula a função extractLinks retornando um array vazio
+    extractLinks.mockReturnValue([]);
+    return expect(mdLinks('arquivo.md', { validate: true })).rejects.toThrow('O arquivo não possui links');
+  });
+  
+  it('deve resolver com estatísticas se options.stats for verdadeiro', () => {
+    extractLinks.mockReturnValue(['https://example.com', 'https://example2.com']);
+    // Mock para simular a função statsLinks
+    statsLinks.mockResolvedValue({
+      total: 2,
+      unique: 2,
+    });
+    return expect(mdLinks('arquivo.md', { stats: true })).resolves.toEqual({
+      total: 2,
+      unique: 2,
+    });
+  });
+  it('deve rejeitar se o arquivo não possui links e options.stats for verdadeiro', () => {
+    // Mock para simular a função extractLinks retornando um array vazio
+    extractLinks.mockReturnValue([]);
+    return expect(mdLinks('arquivo.md', { stats: true })).rejects.toThrow('O arquivo não possui links');
+  });
 
-  // it('deve resolver com estatísticas de links validados se options.validate e options.stats forem verdadeiros', () => {
-  //   // Configurar mocks e valores de retorno
-  // });
+  it('deve resolver com estatísticas de links validados se options.validate e options.stats forem verdadeiros', () => {
+    const links = ['https://example.com', 'https://example2.com'];
+    extractLinks.mockReturnValue(links);
+    // Mock para simular a validação dos links
+    const validatedLinks = links.map((link) => ({
+      text: 'Link de exemplo',
+      url: link,
+      statusCode: 200,
+      statusText: 'OK'
+    }));
+    validateLinks.mockResolvedValue(validatedLinks);
+    // Mock para simular a função statsLinks
+    const statsResult = {
+      total: 2,
+      unique: 2,
+    };
+    statsLinks.mockResolvedValue(statsResult);
+    return mdLinks('arquivo.md', { validate: true, stats: true }).then(() => {
+      // Verifica se validateLinks foi chamada com os links
+      expect(validateLinks).toHaveBeenCalledWith(links);
+      // Verifica se statsLinks foi chamada com os links validados
+      expect(statsLinks).toHaveBeenCalledWith(validatedLinks);
+    });
+  });
+
+  it('deve rejeitar se options.validate e options.stats forem verdadeiros e o arquivo não possui links', () => {
+    // Simula a função extractLinks retornando um array vazio
+    extractLinks.mockReturnValue([]);
+    return expect(mdLinks('arquivo.md', { validate: true, stats: true })).rejects.toThrow('O arquivo não possui links');
+  });
+
+  it('deve rejeitar com a mensagem correta se options.validate e options.stats forem verdadeiros e a validação falhar', () => {
+    const links = ['https://example.com'];
+    extractLinks.mockReturnValue(links);
+    // Simula a função validateLinks rejeitando a promessa com um erro
+    validateLinks.mockRejectedValue(new Error('Erro na validação de links'));
+    return expect(mdLinks('arquivo.md', { validate: true, stats: true })).rejects.toThrow('Erro na validação de links');
+  });
 });
-
-
-
-// const mdLinks = require('../src');
-
-// console.log(mdLinks);
-
-// describe('mdLinks', () => {
-//   it('should...', () => {
-//     console.log('FIX ME!');
-//   });
-// });
