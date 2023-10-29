4da41d4da90cd9ec716616de68efa1c060c8003f
jay santana <jayannyrayara@gmail.com>
2023-10-29 15:27:49 -0300

    Testes unitários index.js

---
+++ test/index.spec.js
@@ -0,0 +1,19 @@
+const { extractLinks, validateLinks, statsLinks } = require('../src/index');
+
+// describe('extractLinks', () => {
+//   it('should...', () => {
+//     console.log('FIX ME!');
+//   });
+// });
+
+// describe('validateLinks', () => {
+//   it('should...', () => {
+//     console.log('FIX ME!');
+//   });
+// });
+
+// describe('statsLinks', () => {
+//   it('should...', () => {
+//     console.log('FIX ME!');
+//   });
+// });
